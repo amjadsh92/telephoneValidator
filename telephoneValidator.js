77f4b006e2d3ed60a6e0d3998bd29a8cb2e099e9
amjadsh92 <amjadsharafeddien@gmail.com>
2025-01-28 20:06:33 +0200

    Finalize the code and format the document

--- telephoneValidator.js
+++ telephoneValidator.js
@@ -1,63 +1,51 @@
-const regex_array=
-    [/^1\s(\d{3})-(\d{3})-(\d{4})$/g, 
-    /^1\s\((\d{3})\)\s(\d{3})-(\d{4})$/g,
-    /^1\((\d{3})\)(\d{3})-(\d{4})$/g,
-    /^1\s(\d{3})\s(\d{3})\s(\d{4})$/g,
-    /^\d{10}$/,
-    /^(\d{3})-(\d{3})-(\d{4})$/g,
-    /^\((\d{3})\)(\d{3})-(\d{4})$/g]
+const regex_array = [
+  /^1\s(\d{3})-(\d{3})-(\d{4})$/g,
+  /^1\s\((\d{3})\)\s(\d{3})-(\d{4})$/g,
+  /^1\((\d{3})\)(\d{3})-(\d{4})$/g,
+  /^1\s(\d{3})\s(\d{3})\s(\d{4})$/g,
+  /^\d{10}$/,
+  /^(\d{3})-(\d{3})-(\d{4})$/g,
+  /^\((\d{3})\)(\d{3})-(\d{4})$/g,
+];
 
+const inputNumber = document.getElementById("input-number");
+const resultContainer = document.getElementById("result");
+const checkButton = document.getElementById("check");
+const clearButton = document.getElementById("clear");
+resultContainer.innerHTML = localStorage.getItem("result-container");
 
-const inputNumber = document.getElementById("input-number") 
-const resultContainer = document.getElementById("result") 
-const checkButton = document.getElementById("check")
-const clearButton = document.getElementById("clear")
-
-resultContainer.innerHTML = localStorage.getItem("result-container")
-
-
-const isValid =(str) => {
-     
-    for ( let reg of regex_array ){
-        if(reg.test(str)){
-            return true
-        }
+const isValid = (str) => {
+  for (let reg of regex_array) {
+    if (reg.test(str)) {
+      return true;
     }
- }
-
- 
-console.log(isValid(inputNumber.value))
-
+  }
+};
 
 const outputResult = () => {
-    if (inputNumber.value === ""){
-        alert("Please provide a phone number")
-    }
-    else if (isValid(inputNumber.value)){
-    const htmlstring = `<p class="valid-input-title">Valid US number:</p><p class="valid-input">${inputNumber.value}</p>`
+  if (inputNumber.value === "") {
+    alert("Please provide a phone number");
+  } else if (isValid(inputNumber.value)) {
+    const htmlstring = `<p class="valid-input-title">Valid US number:</p><p class="valid-input">${inputNumber.value}</p>`;
     resultContainer.innerHTML += htmlstring;
-    localStorage.setItem("result-container", resultContainer.innerHTML)
-    inputNumber.value ="";
-    }
-    else if (!isValid(inputNumber.value)){
-    const htmlstring = `<p class="invalid-input-title">Invalid US number:</p><p class="invalid-input">${inputNumber.value}</p>`
+    localStorage.setItem("result-container", resultContainer.innerHTML);
+    inputNumber.value = "";
+  } else if (!isValid(inputNumber.value)) {
+    const htmlstring = `<p class="invalid-input-title">Invalid US number:</p><p class="invalid-input">${inputNumber.value}</p>`;
     resultContainer.innerHTML += htmlstring;
-    localStorage.setItem("result-container", resultContainer.innerHTML)
-    inputNumber.value ="";
-    }
-    }
+    localStorage.setItem("result-container", resultContainer.innerHTML);
+    inputNumber.value = "";
+  }
+};
 
-
-checkButton.addEventListener("click", outputResult)
-inputNumber.addEventListener("keydown", (event) => {debugger;
-    if(event.key ==="Enter"){outputResult()} 
-    }
-)
-clearButton.addEventListener("click", ()=>{
-     
-     resultContainer.innerHTML = "";
-     localStorage.clear();
-
-
-})
-
+checkButton.addEventListener("click", outputResult);
+inputNumber.addEventListener("keydown", (event) => {
+  debugger;
+  if (event.key === "Enter") {
+    outputResult();
+  }
+});
+clearButton.addEventListener("click", () => {
+  resultContainer.innerHTML = "";
+  localStorage.clear();
+});
