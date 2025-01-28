10ab464b263bc191f1d2bed86046968ff38d5ac6
amjadsh92 <amjadsharafeddien@gmail.com>
2025-01-28 16:20:05 +0200

    Show the result in the result container for valid numbers

--- telephoneValidator.js
+++ telephoneValidator.js
@@ -1,22 +1,34 @@
-const regex_array=[/1\s(\d{3})-(\d{3})-(\d{4})/g, 
+const regex_array=
+    [/1\s(\d{3})-(\d{3})-(\d{4})/g, 
     /1\s\((\d{3})\)\s(\d{3})-(\d{4})/g,
     /1\((\d{3})\)(\d{3})-(\d{4})/g,
     /1\s(\d{3})\s(\d{3})\s(\d{4})/g,
     /\d{10}/,
     /(\d{3})-(\d{3})-(\d{4})/g,
-    /\((\d{3})\)(\d{3})-(\d{4})/g
-    
-    
-
-]
-
-const str = "(555)555-5555"
-
-if(regex_array[6].test(str)){
-    console.log(true)
-
-}
+    /\((\d{3})\)(\d{3})-(\d{4})/g]
 
 
+const inputNumber = document.getElementById("input-number") 
+const resultContainer = document.getElementById("result") 
+const checkButton = document.getElementById("check")
+console.log(inputNumber.value)
 
+const isValid =(str) => {
+     
+    for ( let reg of regex_array ){
+        if(reg.test(str)){
+            return true
+        }
+    }
+ }
+
+ 
+console.log(isValid(inputNumber.value))
+
+
+checkButton.addEventListener("click", () => { debugger; if (isValid(inputNumber.value)){
+    const htmlstring = `<p class="valid-input-title">Valid US number:</p><p class="valid-input">${inputNumber.value}</p>`
+    resultContainer.innerHTML += htmlstring;
+    inputNumber.value ="";
+} } ) 
 
